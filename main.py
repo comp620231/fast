507d8d3b9ff97859916fa607afb9a02a359a3fce
unknown <comp620231@mail.ru>
2023-09-12 12:32:01 +0300

    тело запроса

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import Union
 
 from fastapi import FastAPI
+from enum import Enum
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -12,3 +15,23 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
