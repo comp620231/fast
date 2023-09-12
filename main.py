1d64169699eb4f992319e8473382a52b581bf88a
unknown <comp620231@mail.ru>
2023-09-12 13:44:13 +0300

    проверка

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
-from enum import Enum
 from pydantic import BaseModel
-
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
 
 
 class Item(BaseModel):
@@ -27,7 +9,6 @@ class Item(BaseModel):
     description: Union[str, None] = None
     price: float
     tax: Union[float, None] = None
-
 
 app = FastAPI()
 
